7069a728038fdffee679d3bc981ae1b0d369f0b0
Ben Welsh <b@palewi.re>
2024-03-19 05:44:32 -0400

    Update consolidate.py

--- newshomepages/extract/consolidate.py
+++ newshomepages/extract/consolidate.py
@@ -66,6 +66,8 @@ def consolidate(
     item_list = []
     file_list = []
     for file_name in track(json_list):
+        print(f"- {file_name}")
+        
         # Read the data out of the zip file
         with zf.open(file_name) as fp:
             item_data = json.loads(fp.read())
