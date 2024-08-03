847d07ae4577416b8bcd8808bb1a8570615d3457
palewire <b@palewi.re>
2024-08-03 08:49:03 -0400

    Add a new extract command for the status report

--- newshomepages/extract/cli.py
+++ newshomepages/extract/cli.py
@@ -6,6 +6,7 @@ from .hyperlinks import cli as cli_hyperlinks
 from .items import cli as cli_items
 from .lighthouse import cli as cli_lighthouse
 from .robotstxt import cli as cli_robotstxt
+from .status_report import cli as cli_status_report
 from .wayback import cli as cli_wayback
 
 cli_group = click.CommandCollection(
@@ -16,6 +17,7 @@ cli_group = click.CommandCollection(
         cli_hyperlinks,
         cli_lighthouse,
         cli_robotstxt,
+        cli_status_report,
         cli_wayback,
     ]
 )
